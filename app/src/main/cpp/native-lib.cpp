35906562917a7640f19fc668a7bb79c8a0c56a1a
jose.hidalgo <jose.hidalgo@gps.com.co>
2025-01-27 15:07:59 -0500

    subir proyecto

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,10 @@
 #include <jni.h>
 #include <string>
+#include <fcntl.h>
+#include <unistd.h>
+#include "ql_log.h"
+#define FILE_NAME_LEN (64)
+#include <linux/gpio.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_gps_myapplication_MainActivity_stringFromJNI(
@@ -7,4 +12,175 @@ Java_com_gps_myapplication_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_gps_myapplication_MainActivity_setPermission(JNIEnv *env, jobject /* thiz */) {
+    int result = 0;
+
+    // Check if we have root access
+
+   /*result = system("system/xbin/su");
+    if (result != 0) {
+        LOGE("Root access is not available");
+        return -1;
+    }*/
+
+    // Execute commands with root privileges
+
+    int ret,fd;
+
+    system("su");
+
+    system("su")
+
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (fd < 0) {
+        LOGE("Failed to open export file");
+        return -1;
+    }
+
+    ret = write(fd, "1149", strlen("1149"));
+    if (ret < 0) {
+        LOGE("Failed to write to export file");
+        close(fd);
+        return -1;
+    }
+
+    fd = open("/sys/class/gpio/gpio1149/direction", O_RDWR);
+    if (fd < 0) {
+        LOGE("Failed to open direction file");
+        close(fd);
+        return -1;
+    }
+
+    ret = write(fd, "out", strlen("out"));
+    if (ret < 0) {
+        LOGE("Failed to write to direction file");
+        close(fd);
+        return -1;
+    }
+
+    ret = write(fd, "1", strlen("1"));
+    if (ret < 0) {
+        LOGE("Failed to write to value file");
+        close(fd);
+        return -1;
+    }
+
+
+
+
+
+    close(fd);
+
+
+    //system("/system/xbin/setenforce 0");
+    //system("echo 1149 > /sys/class/gpio/export");
+
+
+
+
+
+
+   /*
+    const char* commands[] = {
+            "setenforce 0",
+            "echo 1149 > /sys/class/gpio/export",
+            "chmod 666 /sys/class/gpio/gpio1149/direction",
+            "chmod 666 /sys/class/gpio/gpio1149/value",
+            "echo out > /sys/class/gpio/gpio1149/direction"
+    };
+
+
+
+    LOGE("Executing commands with root privileges");
+
+    for (const auto& cmd : commands) {
+        LOGE("Executing command: %s", cmd);
+        result = system(cmd);
+        LOGE("Executing command: %s", result);
+        if (result != 0) {
+            LOGE("Failed to execute command: %s", cmd);
+            return -1;
+        }
+    }*/
+
+    return 0;
+}
+
+    /*int fd = -1;
+    char filename[FILE_NAME_LEN];
+    memset(filename,0,sizeof(filename));
+    sprintf(filename,"su");
+    sprintf(filename,"setenforce 0");
+    //export pin 1149
+    sprintf(filename,"echo 1149 > /sys/class/gpio/export");
+    //chmod 666 /sys/class/gpio/gpio1149
+    sprintf(filename,"chmod 666 /sys/class/gpio/gpio1149/direction");
+    sprintf(filename,"chmod 666 /sys/class/gpio/gpio1149/value");
+    sprintf(filename,"echo out > /sys/class/gpio/gpio1149/direction");
+
+    close(fd);
+
+
+
+    return 0;*/
+
+
+
+
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_gps_myapplication_MainActivity_getGPIOValue(JNIEnv *env, jobject thiz, jint gpio_num){
+    // TODO: implement readGPIOValue()
+    int fd = -1;
+    char fileName[FILE_NAME_LEN];
+    memset(fileName,0,sizeof(fileName));
+    sprintf(fileName,"/sys/class/gpio/gpio%d/value",gpio_num);
+    fd= open(fileName,O_RDONLY|O_NONBLOCK);
+    if(fd<0){
+        LOGE("open %s error.",fileName);
+        return -1;
+    }
+    char buf[1];
+    int len = read(fd,buf,1);
+
+    if(len!=1){
+        LOGE("read %s error.",fileName);
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    return buf[0]=='1'?1:0;
+}
+
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_gps_myapplication_MainActivity_setGPIOValue(JNIEnv *env, jobject thiz, jint gpio_num,
+                                                     jint value){
+    // TODO: implement setGPIOValue()
+    int fd=-1;
+    char fileName[FILE_NAME_LEN];
+    memset(fileName,0,sizeof(fileName));
+    sprintf(fileName,"/sys/class/gpio/gpio%d/value",gpio_num);
+    fd = open(fileName,O_RDWR|O_NONBLOCK);
+    if(fd<0){
+        LOGE("open %s error.",fileName);
+        return -1;
+    }
+    char buf[1];
+    buf[0] = 0==value?'0':'1';
+    int len = write(fd,buf,1);
+    if(1!=len){
+        LOGE("write %s error",fileName);
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    return 0;
+
 }
--- app/src/main/java/com/gps/myapplication/MainActivity.kt
+++ app/src/main/java/com/gps/myapplication/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.gps.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.gps.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -15,15 +16,88 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnRead.setOnClickListener(this)
+        binding.btnWrite.setOnClickListener(this)
+
+
+
+       //setPermission()
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        //binding.sampleText.text = stringFromJNI()
     }
+
+
+    override fun onClick(p0: View?) {
+        val id = p0?.id
+        when(id){
+            binding.btnRead.id->{
+                getGPIOValue()
+            }
+
+            binding.btnWrite.id->{
+                setGPIOValue()
+            }
+        }
+    }
+
+
+    /**
+     * Set el valor de gpio numero
+     */
+    private fun setGPIOValue() {
+        val numGpio = binding.etGpionum.text.toString()
+        val valueGpio = binding.etGpiovalue.text.toString()
+        if (numGpio.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese numero de GPIO", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (valueGpio.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese valor para GPIO", Toast.LENGTH_SHORT).show()
+        }
+
+        val resultGpio:Int = setGPIOValue(numGpio.toInt(), valueGpio.toInt())
+
+        if(0!=resultGpio){
+            Toast.makeText(applicationContext, "Error al escribir el pin GPIO", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(applicationContext, "Se escribio con éxito", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+
+
+
+
+    /**
+     * Capturamos el valor de Gpio
+     */
+    private fun getGPIOValue() {
+        val numGpio = binding.etGpionum.text.toString()
+        if (numGpio.isEmpty()){
+            Toast.makeText(applicationContext, "Digite un numero gpio", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val gpioNum = numGpio.toInt()
+        val value = getGPIOValue(gpioNum)
+        if (-1!=value){
+            binding.tvGpiovalue.setText("GPIO ${gpioNum} value is: $value")
+        }else{
+            binding.tvGpiovalue.text = "Error en lectura de GPIO.."
+        }
+
+    }
+
 
     /**
      * A native method that is implemented by the 'myapplication' native library,
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+    external fun getGPIOValue(gpioNum:Int):Int
+    external fun setGPIOValue(gpioNum:Int,value:Int):Int
+    external fun setPermission():Int
 
     companion object {
         // Used to load the 'myapplication' library on application startup.
@@ -31,4 +105,8 @@ class MainActivity : AppCompatActivity() {
             System.loadLibrary("myapplication")
         }
     }
+
+
+
+
 }
